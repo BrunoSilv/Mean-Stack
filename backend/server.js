314f00c58b56d04e616c09888baab6da345a655e
Gil Sousa <tiagosousafl@gmail.com>
2018-11-04 22:56:13 +0000

    Remove unecessary symbols

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ mongoose.connect('mongodb://admin:password1234@ds145463.mlab.com:45463/issues');
 
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully!')
-});
+connection.once('open', _ => console.log('MongoDB database connection established successfully!'));
 
 
 router.route('/issues').get((req, res) => {
@@ -41,35 +39,24 @@ router.route('/issues/:id').get((req, res) => {
 router.route('/issues/add').post((req, res) => {
     let issue = new Issue(req.body);
     issue.save()
-        .then(issue => {
-            res.status(200).json({
-                'issue': 'Added successfully!'
-            });
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create a new record');
-        });
+        .then(_ => res.status(200).json({'issue': 'Added successfully!'}))
+        .catch(_ => res.status(400).send('Failed to create a new record'));
 });
 
 router.route('/issues/update/:id').post((req, res) => {
     Issue.findById(req.params.id, (err, issue) => {
         if (!issue)
             return next(new Error('Could not load document'));
-        else {
-            issue.title = req.body.title;
-            issue.responsible = req.body.responsible;
-            issue.description = req.body.description;
-            issue.severity = req.body.severity;
-            issue.status = req.body.status;
 
-            issue.save()
-                .then(issue => {
-                    res.json('Update Done!');
-                })
-                .catch(err => {
-                    res.status(400).send('Update failed')
-                });
-        }
+        issue.title = req.body.title;
+        issue.responsible = req.body.responsible;
+        issue.description = req.body.description;
+        issue.severity = req.body.severity;
+        issue.status = req.body.status;
+
+        issue.save()
+            .then(_ => res.json('Update Done!'))
+            .catch(_ => res.status(400).send('Update failed'));
     });
 });
 
@@ -77,11 +64,11 @@ router.route('/issues/delete/:id').get((req,res) => {
     Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
         if(err)
             res.json(err);
-        else
-            res.json('Remove successfully!');
+
+        res.json('Remove successfully!');
     });
 });
 
 app.use('/', router);
 
-app.listen(4000, () => console.log('Express server running on port 4000'));
+app.listen(4000, _ => console.log('Express server running on port 4000'));
